3b6c80266aece4ad54a54eda4e10835b02e1cbea
Adam Charnock <adam@omniwiki.co.uk>
2010-08-13 16:03:01 +0100

    initial structure

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+VERSION = __import__('django_badbrowser').__version__
+
+import os
+long_description = open(os.path.join(os.path.dirname(__file__), 'README.textime')).read()
+
+setup(
+      name='django-badbrowser',
+      version=VERSION,
+      url='http://github.com/adamcharnock/django-badbrowser',
+      download_url='git@github.com:adamcharnock/django-badbrowser.git',
+      description='Browser detection (including browser upgrade notices) for Django',
+      long_description=long_description,
+      author='Adam Charnock',
+      author_email='adam@playnice.ly',
+      platforms=['any'],
+      license='MIT',
+      packages=[
+        'django_badbrowser',
+        ],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet :: WWW/HTTP :: Browsers',
+        ],
+      include_package_data=True,
+)
