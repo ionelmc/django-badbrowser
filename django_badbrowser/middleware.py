50345d68decbf13a6ccc1b249ee5690c5c7463ff
Adam Charnock <adam@omniwiki.co.uk>
2010-08-14 14:27:59 +0100

    refactoring out the browser detection logic

--- django_badbrowser/middleware.py
+++ django_badbrowser/middleware.py
@@ -1,38 +1,23 @@
-import httpagentparser
-from pkg_resources import parse_version
-
 from django.conf import settings
+
+from django_badbrowser import check_user_agent
 
 class BrowserSupportDetection(object):
 	def process_request(self, request):
 		
 		if not hasattr(settings, "BADBROWSER_REQUIREMENTS"):
+			return None # no requirements have been setup
+		
+		if "HTTP_USER_AGENT" not in request.META:
 			return None
 		
-		user_agent = request.META['HTTP_USER_AGENT']
-		if not user_agent:
-			return None
+		user_agent = request.META["HTTP_USER_AGENT"]
 		
-		parsed = httpagentparser.detect(user_agent)
+		if check_user_agent(user_agent, settings.BADBROWSER_REQUIREMENTS):
+			return None # continue as normal
+		else:
+			return True # we need to return a response here
 		
-		if "browser" not in parsed:
-			return None
-		
-		if "name" not in parsed["browser"]:
-			return None
-		
-		if "version" not in parsed["browser"]:
-			return None
-		
-		user_browser = parsed["browser"]["name"].lower()
-		user_browser_version = parsed["browser"]["version"]
-		
-		for browser, browser_version in settings.BADBROWSER_REQUIREMENTS:
-			if user_browser == browser.lower():
-				if cmp(parse_version(browser_version), parse_version(user_browser_version)) > 0:
-					return None
-		
-		return True
 	
 	
 
