7d5ec44c0b84d22e72a7417f9f7cd5ee22e189aa
Ionel Maries Cristian <ionel.mc@gmail.com>
2011-04-03 01:01:05 +0300

    Fixed bug with having a browser with "None" version always triggering the warning and changed default behavior to ignore unknown browsers.

--- badbrowser/__init__.py
+++ badbrowser/__init__.py
@@ -28,10 +28,10 @@ def check_user_agent(user_agent, requirements):
     user_browser_version = parsed["browser"]["version"]
     
     for browser, browser_version in requirements:
-        if not browser_version:
-            return False
         if user_browser == browser.lower():
+            if not browser_version:
+                return False
             if cmp(parse_version(browser_version), parse_version(user_browser_version)) <= 0:
                 return True
     
-    return False
+    return True
